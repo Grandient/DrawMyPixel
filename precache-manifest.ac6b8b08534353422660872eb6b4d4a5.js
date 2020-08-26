10915d94a6268d65c4260a061578bbf8fa281884
Gavin <Gavin.Gosling1@gmail.com>
2020-08-25 23:34:09 -0400

    Updates

---
+++ precache-manifest.ac6b8b08534353422660872eb6b4d4a5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71add331bd022ff81a31453c5414935b",
+    "url": "/DrawMyPixel/index.html"
+  },
+  {
+    "revision": "22110d83099b493910ff",
+    "url": "/DrawMyPixel/static/css/2.f502beb6.chunk.css"
+  },
+  {
+    "revision": "66590c518ed138379001",
+    "url": "/DrawMyPixel/static/css/main.c3fa56cf.chunk.css"
+  },
+  {
+    "revision": "22110d83099b493910ff",
+    "url": "/DrawMyPixel/static/js/2.80208f41.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/DrawMyPixel/static/js/2.80208f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66590c518ed138379001",
+    "url": "/DrawMyPixel/static/js/main.478ab7b0.chunk.js"
+  },
+  {
+    "revision": "1737cdad8f9ecd59a130",
+    "url": "/DrawMyPixel/static/js/runtime-main.9fb5dd19.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/DrawMyPixel/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/DrawMyPixel/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/DrawMyPixel/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/DrawMyPixel/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/DrawMyPixel/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/DrawMyPixel/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/DrawMyPixel/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/DrawMyPixel/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/DrawMyPixel/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/DrawMyPixel/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/DrawMyPixel/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/DrawMyPixel/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/DrawMyPixel/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/DrawMyPixel/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/DrawMyPixel/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/DrawMyPixel/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
